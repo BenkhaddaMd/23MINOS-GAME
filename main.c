89830ed67036973a2eec8ec95bf8088cec2894d1
Mohamed BENKHADDA <benkhada111@gmail.com>
2021-12-29 14:21:48 +0100

    re-structure

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <SDL.h>
-#include <SDL_image.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include "structure.h"
 #include "modele.h"
 #include "vue.h"
